bd1f52e8b83b7e2ce03226cce6211bebbb111636
SidathDabare <sidath2007@gmail.com>
2023-06-22 22:52:58 +0200

    completion

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -9,6 +9,7 @@ import { addToCart } from "../redux/actions"
 const ProductPage = () => {
   const [selectedImage, setSelectedImage] = useState(data.images[0])
   const [cartItems, setCartItems] = useState(0)
+  const [showModel, setShowModel] = useState(false)
 
   const cartItemsArr = useSelector((state) => state.cart)
   console.log(cartItemsArr.cart.length)
@@ -51,10 +52,17 @@ const ProductPage = () => {
   }
   useEffect(() => {}, [selectedImage])
   return (
-    <Container className='product-page'>
-      <div className='product-page-left'>
-        <div className='product-page-left-top'>
-          <div className='product-page-left-top-item'>
+    <>
+      <div className={showModel ? "product-model" : "product-model-hide"}>
+        <div className='product-model-content'>
+          <div className='product-model-section01'>
+            <img
+              src='/images/icon-close.svg'
+              alt='close'
+              onClick={() => setShowModel(!showModel)}
+            />
+          </div>
+          <div className='product-model-section02'>
             <div className='previous-div'>
               <img
                 src='/images/icon-previous.svg'
@@ -62,7 +70,11 @@ const ProductPage = () => {
                 onClick={goToPreviousImg}
               />
             </div>
-            <img src={selectedImage.src} alt='product' />
+            <img
+              src={selectedImage.src}
+              alt='product'
+              onClick={() => setShowModel(!showModel)}
+            />
             <div className='next-div'>
               <img
                 src='/images/icon-next.svg'
@@ -71,71 +83,116 @@ const ProductPage = () => {
               />
             </div>
           </div>
+          <div className='product-model-section03'>
+            {data.images.map((image, i) => (
+              <div key={image.id} className='product-page-left-bottom-item'>
+                <img
+                  src={image.src}
+                  alt='product'
+                  onClick={() => setImage(image)}
+                />
+
+                <div
+                  className={
+                    selectedImage.id === image.id
+                      ? "image-overlay-active"
+                      : "image-overlay"
+                  }></div>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className='product-page-left-bottom'>
-          {data.images.map((image, i) => (
-            <div key={image.id} className='product-page-left-bottom-item'>
+      </div>
+      <Container className='product-page'>
+        <div className='product-page-left'>
+          <div className='product-page-left-top'>
+            <div className='product-page-left-top-item'>
+              <div className='previous-div'>
+                <img
+                  src='/images/icon-previous.svg'
+                  alt='next'
+                  onClick={goToPreviousImg}
+                />
+              </div>
               <img
-                src={image.src}
+                src={selectedImage.src}
                 alt='product'
-                onClick={() => setImage(image)}
+                onClick={() => setShowModel(!showModel)}
               />
-
-              <div
-                className={
-                  selectedImage.id === image.id
-                    ? "image-overlay-active"
-                    : "image-overlay"
-                }></div>
+              <div className='next-div'>
+                <img
+                  src='/images/icon-next.svg'
+                  alt='next'
+                  onClick={goToNextImg}
+                />
+              </div>
             </div>
-          ))}
-        </div>
-      </div>
-      <div className='product-page-right'>
-        <div className='product-page-right-content'>
-          <small className='company-name py-2'>
-            {data.company.toUpperCase()}
-          </small>
-          <h1 className='product-name fw-bold py-2'>{data.name}</h1>
-          <p className='company-desc'>{data.description}</p>
-          <div className='d-flex align-items-center product-price'>
-            <h4 className='fw-bold'>
-              ${((data.price * data.discount) / 100).toFixed(2)}
-            </h4>
-            <h6 className='px-3'>{data.discount}%</h6>
           </div>
-          <div className='product-real-price'>
-            <small className='company-name'>${data.price}</small>
-          </div>
-          <div className='quantity-div d-flex align-items-center justify-content-between col-12'>
-            <div className='quantity col-3'>
-              <img
-                src='/images/icon-minus.svg'
-                alt='minus'
-                onClick={decreseItem}
-              />
-              <span className='fw-bold'>{cartItems}</span>
-              <img
-                src='/images/icon-plus.svg'
-                alt='plus'
-                onClick={increseItem}
-              />
-            </div>
+          <div className='product-page-left-bottom'>
+            {data.images.map((image, i) => (
+              <div key={image.id} className='product-page-left-bottom-item'>
+                <img
+                  src={image.src}
+                  alt='product'
+                  onClick={() => setImage(image)}
+                />
 
-            <Button
-              disabled={cartItems === 0 ? true : false}
-              className='add-to-cart d-flex align-items-center justify-content-center px-4 col-6'
-              onClick={() => {
-                dispatch(addToCart(product))
-                setCartItems(0)
-              }}>
-              <img src='/images/icon-cart-white.svg' alt='' />
-              <span className='px-4'>Add to cart</span>
-            </Button>
+                <div
+                  className={
+                    selectedImage.id === image.id
+                      ? "image-overlay-active"
+                      : "image-overlay"
+                  }></div>
+              </div>
+            ))}
           </div>
         </div>
-      </div>
-    </Container>
+        <div className='product-page-right'>
+          <div className='product-page-right-content'>
+            <small className='company-name py-2'>
+              {data.company.toUpperCase()}
+            </small>
+            <h1 className='product-name fw-bold py-2'>{data.name}</h1>
+            <p className='company-desc'>{data.description}</p>
+            <div className='d-flex align-items-center product-price'>
+              <h4 className='fw-bold'>
+                ${((data.price * data.discount) / 100).toFixed(2)}
+              </h4>
+              <h6 className='px-3'>{data.discount}%</h6>
+            </div>
+            <div className='product-real-price'>
+              <small className='company-name'>${data.price}</small>
+            </div>
+            <div className='quantity-div d-flex align-items-center justify-content-between col-12'>
+              <div className='quantity col-3'>
+                <img
+                  src='/images/icon-minus.svg'
+                  alt='minus'
+                  onClick={decreseItem}
+                />
+                <span className='fw-bold'>{cartItems}</span>
+                <img
+                  src='/images/icon-plus.svg'
+                  alt='plus'
+                  onClick={increseItem}
+                />
+              </div>
+
+              <Button
+                disabled={cartItems === 0 ? true : false}
+                className='add-to-cart d-flex align-items-center justify-content-center px-4 col-6'
+                onClick={() => {
+                  dispatch(addToCart(product))
+                  setCartItems(0)
+                }}>
+                <img src='/images/icon-cart-white.svg' alt='' />
+                <span className='px-4'>Add to cart</span>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
   )
 }
 
